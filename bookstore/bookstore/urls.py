9b066c4851be5fc2db90b0bc7fd00fb3343e4784
Adam Jaffe Back <adb33@email.arizona.edu>
2018-02-28 15:51:19 -0800

    (refactor) Moved store routes into its own url.py.

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'bookstore.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'store.views.index', name='index'),
-    url(r'^store/', 'store.views.store', name='store'),
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
