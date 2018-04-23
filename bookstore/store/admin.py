2fcdab648e924fba62628bb1b329019db77deace
Adam Jaffe Back <adb33@email.arizona.edu>
2018-04-23 11:13:11 -0700

    (feat) Added author back to book display in admin.

--- bookstore/store/admin.py
+++ bookstore/store/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Book, Author
 
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('title', 'price', 'stock')
+  list_display = ('title', 'author', 'price', 'stock')
 
 class AuthorAdmin(admin.ModelAdmin):
   list_display = ('last_name', 'first_name')
