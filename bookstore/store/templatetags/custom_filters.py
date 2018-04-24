3adb369833b6e62255c6885b572e4e097b84efa8
Adam Jaffe Back <adb33@email.arizona.edu>
2018-04-24 12:38:58 -0700

    (feat) Added custom filters for Stripe.

---
+++ bookstore/store/templatetags/custom_filters.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='to_cents')
+def to_cents(value):
+  return int(value * 100)
+
+@register.filter(name='pluralize')
+def pluralize(value):
+  if value > 1:
+    return 's'
+  else:
+    return ''
