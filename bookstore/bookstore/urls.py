f6954a5591c8b2375071f98df0504f9c6cb586a7
Adam Jaffe Back <adb33@email.arizona.edu>
2018-05-29 10:43:23 -0400

    (feat) Added media files support.

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^store/', include('store.urls'), name='store'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
