a35c5e9e39d600ec6b243df2d4f7cbcd7acaaa9a
Adam Jaffe Back <adb33@email.arizona.edu>
2018-06-13 14:07:42 -0700

    (feat) Added lat/long field to Review model.

--- bookstore/store/models.py
+++ bookstore/store/models.py
@@ -28,6 +28,8 @@ class Review(models.Model):
   user = models.ForeignKey(User)
   publish_date = models.DateField(default=timezone.now)
   text = models.TextField()
+  latitude = models.FloatField(max_length=20, default='42.208823')
+  longitude = models.FloatField(max_length=20, default='-122.731823')
 
 class Cart(models.Model):
   user = models.ForeignKey(User)
