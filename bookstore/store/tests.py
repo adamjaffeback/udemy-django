2e21c5800897373308ec2dd8430523d181304e5b
Adam Jaffe Back <adb33@email.arizona.edu>
2018-06-20 11:21:06 -0700

    (test) Basic tests for store and cart endpoints.

--- bookstore/store/tests.py
+++ bookstore/store/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Book, Author
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class StoreViewsTestCase(TestCase):
+  def setUp(self):
+    author = Author.objects.create(first_name='Adam', last_name='JB')
+    book = Book.objects.create(
+      title='My Life',
+      author=author,
+      description='Something',
+      price=42,
+      stock=1
+    )
+
+  def test_index(self):
+    resp = self.client.get('/store/')
+    self.assertEqual(resp.status_code, 200)
+    self.assertTrue('books' in resp.context)
+    self.assertTrue(resp.context['books'].count() > 0)
+
+  def test_cart(self):
+    resp = self.client.get('/store/cart/')
+    self.assertEqual(resp.status_code, 302)
