054570950e86f631d8a9713002195539dbc4c229
Adam Jaffe Back <adb33@email.arizona.edu>
2018-06-20 11:27:25 -0700

    (test) Add book to cart.

--- bookstore/store/tests.py
+++ bookstore/store/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from .models import Book, Author
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-
+from decimal import Decimal
 
 class StoreViewsTestCase(TestCase):
   def setUp(self):
+    self.user = User.objects.create_user(
+      username='Adam',
+      email='email@email.com',
+      password='password',
+    )
     author = Author.objects.create(first_name='Adam', last_name='JB')
     book = Book.objects.create(
       title='My Life',
@@ -25,9 +31,19 @@ class StoreViewsTestCase(TestCase):
     self.assertEqual(resp.status_code, 302)
 
   def test_book_detail(self):
-      resp = self.client.get('/store/book/1/')
-      self.assertEqual(resp.status_code, 200)
-      self.assertEqual(resp.context['book'].pk, 1)
-      self.assertEqual(resp.context['book'].title, 'My Life')
-      resp = self.client.get('/store/book/2/')
-      self.assertEqual(resp.status_code, 404)
+    resp = self.client.get('/store/book/1/')
+    self.assertEqual(resp.status_code, 200)
+    self.assertEqual(resp.context['book'].pk, 1)
+    self.assertEqual(resp.context['book'].title, 'My Life')
+    resp = self.client.get('/store/book/2/')
+    self.assertEqual(resp.status_code, 404)
+
+  def test_add_to_cart(self):
+    self.logged_in = self.client.login(username='Adam', password='password')
+    self.assertTrue(self.logged_in)
+    self.client.get('/store/add/1/')
+    resp = self.client.get('/store/cart/')
+    self.assertEqual(resp.context['total'], Decimal('42.00'))
+    self.assertEqual(resp.context['count'], 1)
+    self.assertEqual(resp.context['cart'].count(), 1)
+    self.assertEqual(resp.context['cart'].get().quantity, 1)
